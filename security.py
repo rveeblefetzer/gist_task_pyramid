c79129a54d842bc454b58da9c87b1ea088f1ee48
Rick Valenzuela <devnull@rickv.com>
2017-01-04 12:07:40 -0800

    set initial security policy in __init__.py and security.py

---
+++ security.py
@@ -0,0 +1,15 @@
+import os
+from pyramid.authentication import AuthTktAuthenticationPolicy
+
+
+def includeme(config):
+    """security-related configuration"""
+    auth_secret = os.environ.get('AUTH_SECRET', 'itsaseekrit')
+    authn_policy = AuthTktAuthenticationPolicy(
+        secret=auth_secret,
+        hashalg='sha512'
+    )
+    config.set_authentication_policy(authn_policy)
+    authz_policy = ACLAuthorizationPolicy()
+    config.set_authorization_policy(authz_policy)
+    config.set_default_permission('view')
